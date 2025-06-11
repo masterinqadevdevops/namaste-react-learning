657f89ac5efdb9d8be87d3bd631f7c0551b64cf4
masterinqadevdevops <master.in.qa.dev.devops@gmail.com>
2025-06-11 11:13:39 +0530

    episode-2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // const heading = React.createElement(
 // 	'h1',
 // 	{ id: 'heading' },
@@ -8,13 +10,29 @@
 // root.render(heading);
 
 const parent = React.createElement('div', { id: 'parent' }, [
-	React.createElement('div', { id: 'child' }, [
-		React.createElement('h1', {}, 'I am a child element!'),
-		React.createElement('p', { id: 'para' }, 'This is a paragraph.'),
+	React.createElement('div', { id: 'child', key: 'div-child' }, [
+		React.createElement(
+			'h1',
+			{ id: 'child1', key: 'child ' },
+			'I am a child element! 🚀 Rocket'
+		),
+		React.createElement(
+			'p',
+			{ id: 'para', key: 'para' },
+			'This is a paragraph.'
+		),
 	]),
-	React.createElement('div', { id: 'child2' }, [
-		React.createElement('h2', {}, 'I am a child element'),
-		React.createElement('h3', { id: 'paras' }, 'This is a paragraph'),
+	React.createElement('div', { id: 'child2', key: 'div-child-2' }, [
+		React.createElement(
+			'h2',
+			{ id: 'child3', key: 'child3' },
+			'I am a child element'
+		),
+		React.createElement(
+			'h3',
+			{ id: 'paras', key: 'paras' },
+			'This is a paragraph'
+		),
 	]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
