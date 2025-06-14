98b2b6192ce7eb907ea21e37204c82f606d44719
masterinqadevdevops <master.in.qa.dev.devops@gmail.com>
2025-06-14 13:06:02 +0530

    episode - 3

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// // const JsxHeading = () => (
+// // 	<h1 id="head" tabIndex="5">
+// // 		Namaste React! 🚀
+// // 	</h1>
+// // );
+// // //ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: heading }, 'Namaste React 🚀');
+const HeadingComponent = () => {
+	return (
+		<div id="root">
+			<h1 id="heading">"Hello I am from React heading Compoent 🚀</h1>
+		</div>
+	);
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<HeadingComponent />);
