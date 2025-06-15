f9526426a0934f046d4bc502f9d459d36e80c2b7
masterinqadevdevops <master.in.qa.dev.devops@gmail.com>
2025-06-15 12:58:17 +0530

    episode-4

--- app.js
+++ app.js
@@ -1,23 +1,771 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// // const JsxHeading = () => (
-// // 	<h1 id="head" tabIndex="5">
-// // 		Namaste React! 🚀
-// // 	</h1>
-// // );
-// // //ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const HeadingComponent = () => {
+const Headers = () => {
 	return (
-		<div id="root">
-			<h1 id="heading">"Hello I am from React heading Compoent 🚀</h1>
+		<div className="headers">
+			<div className="logo-container">
+				<img
+					className="brand-logo"
+					src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
+					alt="Logo"
+				/>
+			</div>
+			<div className="nav-items">
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Menu</li>
+					<li>Cart</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+const restList = [
+	{
+		info: {
+			id: '657472',
+			name: 'Burger King',
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2025/3/24/64de6b47-f18a-4d9d-a59c-7c90b9f6b26f_657472.jpg',
+			locality: 'Ramasomayajulu Street',
+			areaName: 'Kakinada',
+			costForTwo: '₹350 for two',
+			cuisines: ['Burgers', 'American'],
+			avgRating: 4.5,
+			parentId: '166',
+			avgRatingString: '4.5',
+			totalRatingsString: '7.4K+',
+			sla: {
+				deliveryTime: 33,
+				lastMileTravel: 7.9,
+				serviceability: 'SERVICEABLE',
+				slaString: '30-35 mins',
+				lastMileTravelString: '7.9 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-16 02:00:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'Rxawards/_CATEGORY-Burger.png',
+						description: 'Delivery!',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'Delivery!',
+									imageId: 'Rxawards/_CATEGORY-Burger.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: '60% OFF',
+				subHeader: 'UPTO ₹120',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/burger-king-ramasomayajulu-street-kakinada-rest657472',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '94898',
+			name: "Domino's Pizza",
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2025/6/14/41f3ce2c-ecde-4e77-aafd-dac2fdc704bb_94898.JPG',
+			locality: 'Bhanugudi',
+			areaName: 'Kakinada',
+			costForTwo: '₹400 for two',
+			cuisines: ['Pizzas', 'Italian', 'Pastas', 'Desserts'],
+			avgRating: 4.4,
+			parentId: '2456',
+			avgRatingString: '4.4',
+			totalRatingsString: '7.8K+',
+			sla: {
+				deliveryTime: 25,
+				lastMileTravel: 5.8,
+				serviceability: 'SERVICEABLE',
+				slaString: '20-25 mins',
+				lastMileTravelString: '5.8 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 22:55:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'Rxawards/_CATEGORY-Pizza.png',
+						description: 'Delivery!',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'Delivery!',
+									imageId: 'Rxawards/_CATEGORY-Pizza.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: 'ITEMS',
+				subHeader: 'AT ₹149',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/dominos-pizza-bhanugudi-kakinada-rest94898',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '240795',
+			name: 'Pizza Hut',
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/6e2902e6-ea0d-467d-b343-9b6293e91860_240795.JPG',
+			locality: 'SRMT Mall',
+			areaName: 'Kakinada',
+			costForTwo: '₹350 for two',
+			cuisines: ['Pizzas'],
+			avgRating: 4.3,
+			parentId: '721',
+			avgRatingString: '4.3',
+			totalRatingsString: '4.0K+',
+			sla: {
+				deliveryTime: 39,
+				lastMileTravel: 2.8,
+				serviceability: 'SERVICEABLE',
+				slaString: '35-40 mins',
+				lastMileTravelString: '2.8 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 23:00:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'Rxawards/_CATEGORY-Pizza.png',
+						description: 'Delivery!',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'Delivery!',
+									imageId: 'Rxawards/_CATEGORY-Pizza.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: 'ITEMS',
+				subHeader: 'AT ₹59',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/pizza-hut-srmt-mall-kakinada-rest240795',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '747954',
+			name: 'NIC Ice Creams',
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2024/11/22/72abd4cf-7d78-4a28-b226-f5078c4dd590_747954.jpg',
+			locality: 'Sarpavaram Junction',
+			areaName: 'SRMT Mall',
+			costForTwo: '₹120 for two',
+			cuisines: ['Ice Cream', 'Desserts'],
+			avgRating: 4.6,
+			veg: true,
+			parentId: '6249',
+			avgRatingString: '4.6',
+			totalRatingsString: '814',
+			sla: {
+				deliveryTime: 22,
+				lastMileTravel: 2.8,
+				serviceability: 'SERVICEABLE',
+				slaString: '20-25 mins',
+				lastMileTravelString: '2.8 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 22:30:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'v1695133679/badges/Pure_Veg111.png',
+						description: 'pureveg',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'pureveg',
+									imageId: 'v1695133679/badges/Pure_Veg111.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: '50% OFF',
+				subHeader: 'UPTO ₹100',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/nic-ice-creams-sarpavaram-junction-srmt-mall-rest747954',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '237670',
+			name: "McDonald's",
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2025/6/3/0656ed17-b50e-4ae1-b5e6-6ac1f3ca6db5_237670.jpg',
+			locality: 'SRMT Mall',
+			areaName: 'Kakinada',
+			costForTwo: '₹400 for two',
+			cuisines: ['Burgers', 'Beverages', 'Cafe', 'Desserts'],
+			avgRating: 4.3,
+			parentId: '630',
+			avgRatingString: '4.3',
+			totalRatingsString: '6.9K+',
+			sla: {
+				deliveryTime: 29,
+				lastMileTravel: 2.8,
+				serviceability: 'SERVICEABLE',
+				slaString: '25-30 mins',
+				lastMileTravelString: '2.8 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 23:00:00',
+				opened: true,
+			},
+			badges: {},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: '₹80 OFF',
+				subHeader: 'ABOVE ₹349',
+				discountTag: 'FLAT DEAL',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/mcdonalds-srmt-mall-kakinada-rest237670',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '435808',
+			name: 'Kwality Walls Ice Cream and More',
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/e4024d21-cfea-4237-a1fa-a95312227f9d_435808.JPG',
+			locality: 'Bhanugudi',
+			areaName: 'Kakinada Locality',
+			costForTwo: '₹300 for two',
+			cuisines: ['Desserts', 'Ice Cream', 'Ice Cream Cakes'],
+			avgRating: 4.6,
+			veg: true,
+			parentId: '582',
+			avgRatingString: '4.6',
+			totalRatingsString: '1.8K+',
+			sla: {
+				deliveryTime: 34,
+				lastMileTravel: 7.9,
+				serviceability: 'SERVICEABLE',
+				slaString: '30-35 mins',
+				lastMileTravelString: '7.9 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 20:00:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'v1695133679/badges/Pure_Veg111.png',
+						description: 'pureveg',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'pureveg',
+									imageId: 'v1695133679/badges/Pure_Veg111.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: '60% OFF',
+				subHeader: 'UPTO ₹120',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/kwality-walls-ice-cream-and-more-bhanugudi-kakinada-locality-rest435808',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '613182',
+			name: 'Rajugari Vindhubojanam',
+			cloudinaryImageId:
+				'RX_THUMBNAIL/IMAGES/VENDOR/2025/5/29/0507d630-51a4-4c59-a0e0-0d6bf06c05a5_613182 (1).jpg',
+			locality: 'Kakinada',
+			areaName: 'Kakinada',
+			costForTwo: '₹349 for two',
+			cuisines: ['Biryani', 'Chinese', 'South Indian'],
+			avgRating: 4.2,
+			parentId: '365786',
+			avgRatingString: '4.2',
+			totalRatingsString: '5.5K+',
+			sla: {
+				deliveryTime: 24,
+				lastMileTravel: 4.9,
+				serviceability: 'SERVICEABLE',
+				slaString: '20-25 mins',
+				lastMileTravelString: '4.9 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 23:00:00',
+				opened: true,
+			},
+			badges: {},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: 'ITEMS',
+				subHeader: 'AT ₹89',
+				discountCalloutInfo: {
+					message: 'Free Delivery',
+					logoCtx: {
+						logo: 'v1655895371/free_delivery_logo_hqipbo.png',
+					},
+				},
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/rajugari-vindhubojanam-kakinada-rest613182',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '93929',
+			name: 'Subbayya Gari Hotel OLD & ORIGINAL',
+			cloudinaryImageId: 'bdgtgldczdeo0dkexatc',
+			locality: 'Rama Rao Peta',
+			areaName: 'Rama Rao Peta',
+			costForTwo: '₹199 for two',
+			cuisines: ['South Indian'],
+			avgRating: 4.4,
+			veg: true,
+			parentId: '12737',
+			avgRatingString: '4.4',
+			totalRatingsString: '18K+',
+			sla: {
+				deliveryTime: 30,
+				lastMileTravel: 7.5,
+				serviceability: 'SERVICEABLE',
+				slaString: '30-35 mins',
+				lastMileTravelString: '7.5 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 16:00:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'Rxawards/_CATEGORY-South%20Indian.png',
+						description: 'Delivery!',
+					},
+				],
+			},
+			isOpen: true,
+			aggregatedDiscountInfoV2: {},
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'Delivery!',
+									imageId: 'Rxawards/_CATEGORY-South%20Indian.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/subbayya-gari-hotel-old-and-original-rama-rao-peta-rest93929',
+			type: 'WEBLINK',
+		},
+	},
+	{
+		info: {
+			id: '93907',
+			name: 'Yati',
+			cloudinaryImageId: 'u2uui5oez6yuzbq10cwb',
+			locality: 'Kakinada',
+			areaName: 'Bhanugudi',
+			costForTwo: '₹400 for two',
+			cuisines: ['Biryani', 'South Indian', 'North Indian', 'Chinese'],
+			avgRating: 4.2,
+			parentId: '557800',
+			avgRatingString: '4.2',
+			totalRatingsString: '21K+',
+			sla: {
+				deliveryTime: 35,
+				lastMileTravel: 5.9,
+				serviceability: 'SERVICEABLE',
+				slaString: '30-35 mins',
+				lastMileTravelString: '5.9 km',
+				iconType: 'ICON_TYPE_EMPTY',
+			},
+			availability: {
+				nextCloseTime: '2025-06-15 22:15:00',
+				opened: true,
+			},
+			badges: {
+				imageBadges: [
+					{
+						imageId: 'Rxawards/_CATEGORY-Biryani.png',
+						description: 'Delivery!',
+					},
+				],
+			},
+			isOpen: true,
+			type: 'F',
+			badgesV2: {
+				entityBadges: {
+					imageBased: {
+						badgeObject: [
+							{
+								attributes: {
+									description: 'Delivery!',
+									imageId: 'Rxawards/_CATEGORY-Biryani.png',
+								},
+							},
+						],
+					},
+					textBased: {},
+					textExtendedBadges: {},
+				},
+			},
+			aggregatedDiscountInfoV3: {
+				header: 'ITEMS',
+				subHeader: 'AT ₹55',
+			},
+			differentiatedUi: {
+				displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+				differentiatedUiMediaDetails: {
+					lottie: {},
+					video: {},
+				},
+			},
+			reviewsSummary: {},
+			displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+			restaurantOfferPresentationInfo: {},
+			externalRatings: {
+				aggregatedRating: {
+					rating: '--',
+				},
+			},
+			ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+		},
+		analytics: {
+			context: 'seo-data-9f471e61-1e53-4df3-9a03-f588cda5626e',
+		},
+		cta: {
+			link: 'https://www.swiggy.com/city/kakinada/yati-bhanugudi-rest93907',
+			type: 'WEBLINK',
+		},
+	},
+];
+const Body = () => {
+	return (
+		<div className="body">
+			<div className="search">search bar</div>
+			<div className="restro-cards-container">
+				{restList.map((restaurant) => (
+					<RestroCards key={restaurant.info.id} resData={restaurant} />
+				))}
+			</div>
+		</div>
+	);
+};
+const RestroCards = (prop) => {
+	const { resData } = prop;
+	const { cloudinaryImageId, name, cuisines, avgRating, deliveryTime } =
+		resData?.info;
+	return (
+		<div className="restro-cards" style={{ backgroundColor: '#f0f0f0' }}>
+			<img
+				className="rest-img"
+				src={
+					'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' +
+					cloudinaryImageId
+				}
+				alt="KFC"
+			/>
+			<h3>{name}</h3>
+			<h3>{cuisines.join(',')}</h3>
+			<h3>{avgRating} Stars</h3>
+			<h3>{resData.info.sla.deliveryTime} Minutes</h3>
+		</div>
+	);
+};
+const AppLayout = () => {
+	return (
+		<div className="app">
+			<Headers />
+			<Body />
 		</div>
 	);
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent />);
+ReactDOM.createRoot(document.getElementById('root')).render(<AppLayout />);
